95853c8673aacc8e7d1ea15af2875275513cf88f
Julian McNeill <julesdowork@gmail.com>
2019-10-28 21:37:18 -0400

    Creating Our Level and BaseController Script

---
+++ Awesome Zombie Crasher/Assets/Scripts/Player/BaseController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    public Vector3 speed;
+    public float xSpeed = 8f, zSpeed = 15f;
+    public float accelerateForce = 15f, decelerationForce = -10f;
+    public float lowSoundPitch, normalSoundPitch, highSoundPitch;
+    public AudioClip engineOnSound, engineOffSound;
+
+    protected float rotationSpeed = 10f;
+    protected float maxAngle = 10f;
+
+    private bool isSlow;
+
+    AudioSource soundManager;
+
+    void Awake()
+    {
+        speed = new Vector3(0, 0, zSpeed);
+        soundManager = GetComponent<AudioSource>();
+    }
+
+    protected void MoveLeft()
+    {
+        speed = new Vector3(-xSpeed / 2f, 0f, speed.z);  // divide by 2 so we don't go too fast
+    }
+
+    protected void MoveRight()
+    {
+        speed = new Vector3(xSpeed / 2f, 0f, speed.z);  // divide by 2 so we don't go too fast
+    }
+
+    protected void MoveStraight()
+    {
+        speed = new Vector3(0f, 0f, speed.z);
+    }
+
+    protected void MoveNormal()
+    {
+        if (isSlow)
+        {
+            isSlow = false;
+
+            soundManager.Stop();
+            soundManager.clip = engineOnSound;
+            soundManager.volume = 0.3f;
+            soundManager.Play();
+        }
+        speed = new Vector3(speed.x, 0f, zSpeed);
+    }
+
+    protected void MoveSlow()
+    {
+        if (!isSlow)
+        {
+            isSlow = true;
+
+            soundManager.Stop();
+            soundManager.clip = engineOffSound;
+            soundManager.volume = 0.5f;
+            soundManager.Play();
+        }
+        speed = new Vector3(speed.x, 0f, decelerationForce);
+    }
+
+    protected void MoveFast()
+    {
+        speed = new Vector3(speed.x, 0f, accelerateForce);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Awesome Zombie Crasher
 
+### To-Do
++ Add different kinds of player tanks
+
 ### About this Game
 From the course [Unity Game Development: Create 2D and 3D Games With C#](https://www.udemy.com/course/unity-game-development-create-2d-and-3d-games-with-c/) by Awesome Tuts.
