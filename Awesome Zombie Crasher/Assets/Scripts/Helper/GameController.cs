95e2481aef596b9ca1e99de637f162c9258382fb
Julian McNeill <julesdowork@gmail.com>
2019-10-29 22:09:30 -0400

    Spawning Obstacles in Our Game

---
+++ Awesome Zombie Crasher/Assets/Scripts/Helper/GameController.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController instance;
+
+    public GameObject[] obstaclePrefabs;
+    public GameObject[] zombiePrefabs;
+    public Transform[] lanes;
+    public float minObstacleDelay = 10f, maxObstacleDelay = 40f;
+
+    private float halfGroundSize;
+
+    BaseController playerController;
+
+    void Awake()
+    {
+        MakeInstance();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        halfGroundSize = GameObject.Find("GroundBlock Main").GetComponent<GroundBlock>().halfLength;
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<BaseController>();
+
+        StartCoroutine("GenerateObstacles");
+    }
+
+    private void MakeInstance()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+    }
+
+    private IEnumerator GenerateObstacles()
+    {
+        float timer = Random.Range(minObstacleDelay, maxObstacleDelay) / playerController.speed.z;
+        yield return new WaitForSeconds(timer);
+
+        CreateObstacles(playerController.gameObject.transform.position.z + halfGroundSize);
+
+        StartCoroutine("GenerateObstacles");
+    }
+
+    private void CreateObstacles(float zPos)
+    {
+        int r = Random.Range(0, 10);
+
+        if (0 <= r && r < 7)
+        {
+            int obstacleLane = Random.Range(0, lanes.Length);
+
+            AddObstacle(new Vector3(lanes[obstacleLane].transform.position.x, 0, zPos), Random.Range(0, obstaclePrefabs.Length));
+
+            int zombieLane = 0;
+
+            if (obstacleLane == 0)
+            {
+                zombieLane = Random.Range(0, 2) == 1 ? 1 : 2;
+            }
+            else if (obstacleLane == 1)
+            {
+                zombieLane = Random.Range(0, 2) == 1 ? 0 : 2;
+            }
+            else if (obstacleLane == 2)
+            {
+                zombieLane = Random.Range(0, 2) == 1 ? 1 : 0;
+            }
+
+            AddZombies(new Vector3(lanes[zombieLane].transform.position.x, 0, zPos));
+        }
+    }
+
+    private void AddObstacle(Vector3 pos, int type)
+    {
+        GameObject obstacle = Instantiate(obstaclePrefabs[type], pos, Quaternion.identity);
+        bool mirror = Random.Range(0, 2) == 1;
+
+        switch (type)
+        {
+            case 0:
+                obstacle.transform.rotation = Quaternion.Euler(0, mirror ? -20f : 20f, 0);
+                break;
+            case 1:
+                obstacle.transform.rotation = Quaternion.Euler(0, mirror ? -20f : 20f, 0);
+                break;
+            case 2:
+                obstacle.transform.rotation = Quaternion.Euler(0, mirror ? -1f : 1f, 0);
+                break;
+            case 3:
+                obstacle.transform.rotation = Quaternion.Euler(0, mirror ? -170f : 170f, 0);
+                break;
+        }
+
+        obstacle.transform.position = pos;
+    }
+
+    private void AddZombies(Vector3 pos)
+    {
+        int count = Random.Range(0, 3) + 1;
+
+        for (int i = 0; i < count; i++)
+        {
+            Vector3 shift = new Vector3(Random.Range(-0.5f, 0.5f), 0, Random.Range(1f, 10f) * i);
+            Instantiate(zombiePrefabs[Random.Range(0, zombiePrefabs.Length)],
+                pos + shift * i, Quaternion.identity);
+        }
+    }
+}
